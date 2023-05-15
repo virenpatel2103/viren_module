38df56f4c0873487335d0ce225731c5b6d8cf037
virenpatel2103 <viren.patel@icreativetechnolabs.com>
2023-05-15 10:06:34 +0530

    [UPDATED]15/05

--- models/inherit_sale_order_model.py
+++ models/inherit_sale_order_model.py
@@ -6,10 +6,17 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     # combo_id = fields.Many2one('product.combo', string="Select combo")
-    combo_id = fields.Char("iiiinnnnnhhheerriit")
+    combo_id = fields.Many2one('product.combo', string="Select combo")
 
-#
-
-
-
-
+    @api.onchange('combo_id')
+    def _onchange_combo_id(self):
+        for rec in self:
+            if rec.order_line == False:
+                print('----------------------------')
+            else:
+                print('***************************')
+                if rec.combo_id != False:
+                    data_search = self.env['product.combo'].search([('id', '=', rec.combo_id.id)])
+                    lst = []
+                    for line in data_search.sub_product_ids:
+                        lst.append((0,0, {'product_id': line.id}))
--- models/product_combo.py
+++ models/product_combo.py
@@ -6,5 +6,5 @@ class ProductCombo(models.Model):
     _name = "product.combo"
     _rec_name = "name"
 
-    name = fields.Char(string="Combo name")
-    sub_product_ids = fields.Many2many('product.template', string="Select products")
+    name = fields.Char(string="Combo name", required=True)
+    sub_product_ids = fields.Many2many('product.template', string="Select products", required=True)
