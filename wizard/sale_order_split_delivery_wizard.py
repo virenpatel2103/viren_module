1cc1f77c19a57e7cf60168b30214150bf415d198
virenvirenpatel <viren.patel.icreativetechnolabs@gmail.com>
2023-05-22 13:23:40 +0530

    [UPDATED]

--- wizard/sale_order_split_delivery_wizard.py
+++ wizard/sale_order_split_delivery_wizard.py
@@ -10,9 +10,42 @@ class SplitDeliveryWizard(models.TransientModel):
     product_product_ids = fields.One2many('split.product', 'ref_id',
                                           string="Details")
 
-    def action_confirm(self):
+    def action_action_confirm(self):
+        print(self._context)
+
         stock_data = self.env['stock.picking'].search \
             ([('sale_id', '=', self._context['sale_order_id'])])
+        print(stock_data)
+        new_data_one = stock_data.copy({
+            'move_ids_without_package': False,
+            'sale_id': self._context['sale_order_id']
+        })
+        print(new_data_one)
+        new_data_two = stock_data.copy({
+            'move_ids_without_package': False,
+            'sale_id': self._context['sale_order_id']
+        })
+        print()
+        print(new_data_two)
+        for lines in self.product_product_ids:
+            if lines.split_choice == True:
+                for rec in stock_data.move_ids_without_package:
+                    if rec.product_id == lines.product_id:
+                        rec.copy({
+                            'picking_id': new_data_one.id
+                        })
+            else:
+                for rec in stock_data.move_ids_without_package:
+                    if rec.product_id == lines.product_id:
+                        rec.copy({
+                            'picking_id': new_data_two.id
+                        })
+
+        stock_data.action_cancel()
+        new_data_one.action_confirm()
+        new_data_two.action_confirm()
+
+
 
 
     class SplitProduct(models.TransientModel):
